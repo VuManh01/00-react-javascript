76693f65fffa89c9db2d1adbf52aabe7b84ef8f5
VuManh01 <vutrongmanhmk@gmail.com>
2024-09-24 10:47:04 +0700

    Final

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,14 @@
+import { CrownOutlined } from '@ant-design/icons';
+import { Result } from 'antd';
 
 const HomePage = () => {
     return (
-        <>
-        HomePage
-        </>
+        <div style={{ padding: 20 }}>
+            <Result
+                icon={<CrownOutlined />}
+                title="JSON Web Token (React/Node.JS) - createdBy @hoidanit"
+            />
+        </div>
     )
 }
 
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,94 +1,100 @@
-import React, { useContext } from "react";
-import { Button, Form, Input, notification } from "antd";
-import { loginApi } from "../util/api";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../../components/context/auth.context";
+import React, { useContext } from 'react';
+import { Button, Col, Divider, Form, Input, notification, Row } from 'antd';
+import { loginApi } from '../util/api';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../components/context/auth.context';
+import { ArrowLeftOutlined } from '@ant-design/icons';
 
 const LoginPage = () => {
-  const navigate = useNavigate();
-  const { setAuth } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const { setAuth } = useContext(AuthContext);
 
-  const onFinish = async (values) => {
-    const { email, password } = values;
+    const onFinish = async (values) => {
+        const { email, password } = values;
 
-    const res = await loginApi(email, password);
+        const res = await loginApi(email, password);
 
-    if (res && res.ErrorCode === 0) {
-      localStorage.setItem("access_token", res.access_token);
-      notification.success({
-        //"CREATE USER"
-        message: "Đăng Nhập Thành Công",
-        // Success
-        description: "Hoàn Thành",
-      });
-      setAuth({
-        isAuthenticated: true,
-        user: {
-          email: res?.user?.email ?? "",
-          name: res?.user?.name ?? "",
-        },
-      });
-      navigate("/");
-    } else {
-      notification.error({
-        message: "Đăng Nhập Thất Bại",
-        description: res?.ErrorMessage ?? "Lỗi Đăng Nhập",
-      });
-    }
+        // Sử dụng ErrorCode và ErrorMessage thay vì EC và EM
+        if (res && res.ErrorCode === 0) {
+            localStorage.setItem("access_token", res.access_token);
+            notification.success({
+                message: "Đăng Nhập Thành Công",
+                description: "Hoàn Thành"
+            });
+            setAuth({
+                isAuthenticated: true,
+                user: {
+                    email: res?.user?.email ?? "",
+                    name: res?.user?.name ?? ""
+                }
+            });
+            navigate("/");
 
-    console.log(">> Success:", res);
-  };
+        } else {
+            notification.error({
+                message: "Đăng Nhập Thất Bại",
+                description: res?.ErrorMessage ?? "Lỗi Đăng Nhập"
+            });
+        }
+    };
 
-  return (
-    <div style={{ margin: 85 }}>
-      <Form
-        name="basic"
-        labelCol={{
-          span: 8,
-        }}
-        wrapperCol={{
-          span: 16,
-        }}
-        style={{
-          maxWidth: 600,
-        }}
-        onFinish={onFinish}
-        autoComplete="off"
-        layout="vertical"
-      >
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            {
-              required: true,
-              message: "Please input your email!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your password!",
-            },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
+    return (
+        <Row justify={"center"} style={{ marginTop: "30px" }}>
+            <Col xs={24} md={16} lg={8}>
+                <fieldset style={{
+                    padding: "15px",
+                    margin: "5px",
+                    border: "1px solid #ccc",
+                    borderRadius: "5px"
+                }}>
+                    <legend>Đăng Nhập</legend>
+                    <Form
+                        name="basic"
+                        onFinish={onFinish}
+                        autoComplete="off"
+                        layout='vertical'
+                    >
+                        <Form.Item
+                            label="Email"
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your email!',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Login
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
-  );
-};
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your password!',
+                                },
+                            ]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit">
+                                Login
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                    <Link to={"/"}><ArrowLeftOutlined /> Quay lại trang chủ</Link>
+                    <Divider />
+                    <div style={{ textAlign: "center" }}>
+                        Chưa có tài khoản? <Link to={"/register"}>Đăng ký tại đây</Link>
+                    </div>
+                </fieldset>
+            </Col>
+        </Row>
+    )
+}
 
 export default LoginPage;
